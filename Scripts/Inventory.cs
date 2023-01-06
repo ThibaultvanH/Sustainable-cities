f06a65b95de1901e796107dbc237867b8fd2267d
Lenme-exe <lenmertens11@gmail.com>
2023-01-06 20:00:24 +0100

    Simple inventory system

---
+++ Scripts/Inventory.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private static float saplingCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void AddSapling()
+    {
+        saplingCount++;
+    }
+    public static void RemoveSapling()
+    {
+        if (saplingCount > 0)
+            saplingCount--;
+        else 
+            throw new System.Exception("Negative amount of saplings");
+    }
+}
