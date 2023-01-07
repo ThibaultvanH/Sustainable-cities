adba4ddd2c66e0050b50ae409437aeec06760812
Lenme-exe <lenmertens11@gmail.com>
2023-01-07 02:50:14 +0100

    Tree grow fast as fuck

--- Scripts/Planter.cs
+++ Scripts/Planter.cs
@@ -10,7 +10,8 @@ public class Planter : MonoBehaviour
     
     
     private bool spawned;
-    private bool planted;
+    public bool planted;
+    private GameObject clonedSapling;
     
     // Start is called before the first frame update
     void Start()
@@ -27,15 +28,15 @@ public class Planter : MonoBehaviour
         {
             if(!spawned)
             {
-                Instantiate(sapling, spawnPos.position, Quaternion.identity);
-                sapling.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                clonedSapling = Instantiate(sapling, spawnPos.position, Quaternion.identity);
+                clonedSapling.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
                 spawned = true;
             }
             else 
             {
-                while(sapling.transform.localScale.x < 1)
+                while (clonedSapling.transform.localScale.x < 1)
                 {
-                    sapling.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f) * Time.deltaTime * growSpeed;
+                    clonedSapling.transform.localScale += new Vector3(0.01f, 0.01f, 0.01f) * Time.deltaTime * growSpeed;
                 }
             }
         }
